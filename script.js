5abb5745a37c09d57f9508ed1f7f448bf764ca20
cschmidt216 <iamdrainy@gmail.com>
2023-04-02 15:21:05 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const APIkey = "8393c34c85fc484cc3f536aeb531b29c";
+
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('#search');
+const currentWeather = document.querySelector('#current-weather');
+const forecast = document.querySelector('#forecast');
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const searchTerm = searchInput.value;
+  getWeather(searchTerm);
+});
+
+async function getWeather(searchTerm) {
+  const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=imperial&appid=${APIkey}`;
+  const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&units=imperial&appid=${APIkey}`;
+
+  try {
+    const currentWeatherResponse = await fetch(currentWeatherURL);
+    const currentWeatherData = await currentWeatherResponse.json();
+
+    const forecastResponse = await fetch(forecastURL);
+    const forecastData = await forecastResponse.json();
+
+    showWeather(currentWeatherData, forecastData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function showWeather(currentWeatherData, forecastData) {
+  // Show current weather
+  const temperature = Math.round(currentWeatherData.main.temp);
+  const description = currentWeatherData.weather[0].description;
+  const cityName = currentWeatherData.name;
+  const countryName = currentWeatherData.sys.country;
+
+  currentWeather.innerHTML = `
+    <h2>${cityName}, ${countryName}</h2>
+    <p>${description}</p>
+    <p>${temperature}°F</p>
+  `;
+
+  // Show forecast
+  const forecastDays = forecastData.list.filter((weather) => weather.dt_txt.includes('12:00:00'));
+  forecast.innerHTML = '';
+
+  forecastDays.forEach((day) => {
+    const dayName = new Date(day.dt * 1000).toLocaleDateString('en-US', { weekday: 'long' });
+    const temperature = Math.round(day.main.temp);
+    const iconURL = `https://openweathermap.org/img/w/${day.weather[0].icon}.png`;
+
+    const forecastDay = document.createElement('div');
+    forecastDay.classList.add('forecast-day');
+    forecastDay.innerHTML = `
+      <h2>${dayName}</h2>
+      <img src="${iconURL}" alt="${day.weather[0].description}">
+      <p>${temperature}°F</p>
+    `;
+
+    forecast.appendChild(forecastDay);
+  });
+
+  // Show weather info container
+  const weatherInfo = document.querySelector('#weather-info');
+  weatherInfo.style.display = 'block';
+}
