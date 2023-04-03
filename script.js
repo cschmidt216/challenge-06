5d32b1ef9ccaf5c5e76df0fcdeb48eb06b027b78
cschmidt216 <iamdrainy@gmail.com>
2023-04-03 12:46:10 -0700

    added search history

--- script.js
+++ script.js
@@ -73,3 +73,62 @@ function showWeather(currentWeatherData, forecastData) {
   const weatherInfo = document.querySelector('#weather-info');
   weatherInfo.style.display = 'block';
 }
+
+// Initialize the search history array
+let searchHistory = [];
+
+// Check if search history is already in local storage
+if (localStorage.getItem('searchHistory')) {
+  searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
+}
+
+// Add a submit event listener to the form
+searchForm.addEventListener('submit', function(e) {
+  e.preventDefault(); // Prevent the form from submitting
+
+  // Get the search term from the input element
+  const searchTerm = searchInput.value;
+
+  // Add the search term to the search history array
+  searchHistory.push(searchTerm);
+
+  // Save the search history array to local storage
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+
+  // Reset the form
+  searchForm.reset();
+
+  // Update the search history dropdown
+  updateDropdown();
+});
+
+// Function to update the search history dropdown
+function updateDropdown() {
+  const dropdown = document.createElement('select');
+  dropdown.id = 'searchHistoryDropdown';
+
+  // Add the default "Select a city" option
+  const defaultOption = document.createElement('option');
+  defaultOption.text = 'Select a city';
+  defaultOption.disabled = true;
+  defaultOption.selected = true;
+  dropdown.add(defaultOption);
+
+  // Add each search term to the dropdown
+  searchHistory.forEach(function(searchTerm) {
+    const option = document.createElement('option');
+    option.text = searchTerm;
+    dropdown.add(option);
+  });
+
+  // Replace the existing dropdown (if any) with the new one
+  const existingDropdown = document.querySelector('#searchHistoryDropdown');
+  if (existingDropdown) {
+    existingDropdown.replaceWith(dropdown);
+  } else {
+    searchForm.prepend(dropdown);
+  }
+}
+
+// Call the updateDropdown function to initially populate the dropdown
+updateDropdown();
